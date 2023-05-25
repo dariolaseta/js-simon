593af4fb995718b0cf76cff630d820286e4f4d13
dariolaseta <dario.laseta@gmail.com>
2023-05-25 15:13:03 +0200

    added script for counting hours

--- js/script.js
+++ js/script.js
@@ -6,4 +6,8 @@ const timer = setInterval(function(){
     
     let days =  Math.floor(remainingTime / (1000 * 60 * 60 * 24));
     document.getElementById("days").innerHTML = days;
-});
+
+    let hours = Math.floor(remainingTime % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    document.getElementById("hours").innerHTML = hours;
+
+}, 100);
