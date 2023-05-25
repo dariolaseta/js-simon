f89bf2c9abb6edb91c2a09c6a4667187bea79cc8
dariolaseta <dario.laseta@gmail.com>
2023-05-25 15:02:15 +0200

    created varibales for timer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
-console.log("Script loaded");
+const nextDayTimer = new Date("May 25, 2023 09:30:00").getTime();
+let currentTime = new Date().getTime();
+let remainingTime = nextDayTimer - currentTime;
+
